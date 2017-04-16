26b653579924e9ac2c7dcd89f64ab7b8c570933f
Stefan K <stefankoteski8@yahoo.com>
2017-04-16 18:43:33 +0200

    Added confimation flag on install for zsh and terminator

--- install-zsh.sh
+++ install-zsh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Installing ZSH and terminator"
-sudo apt-get install zsh terminator
+sudo apt-get install -y zsh terminator
 
 echo "Installing Oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
